32a49dda82d8ca11576ae6dd0ddede1653bc2419
Rabab-Mahgoub <rababmahgoub5@gmail.com>
2021-11-03 19:56:48 +0200

    first commit

--- assets/js/blood_Bank.js
+++ assets/js/blood_Bank.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+   
+    function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    };
+  
+
+    
+
     var sliider =$('#slide');
 sliider.owlCarousel({
     loop:true,
@@ -28,4 +40,5 @@ sliider.owlCarousel({
     }
     }
     );
+   
 });
